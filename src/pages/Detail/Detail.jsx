acc19db3c4f44da00e4bbd1bc61273f870db1601
Tài Cẩn <taicancr7@gmail.com>
2022-04-01 12:13:57 +0700

    Init commit

---
+++ src/pages/Detail/Detail.jsx
@@ -0,0 +1,179 @@
+import React, { useState, useEffect } from "react";
+import "./detail.scss";
+import axios from "axios";
+import fileDownload from "js-file-download";
+
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+
+import Button from "@mui/material/Button";
+
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+
+import Chart from "chart.js/auto";
+// import * as XLSX from "xlsx/xlsx.mjs";
+// import * as XLSX from "https://unpkg.com/xlsx/xlsx.mjs";
+import { SaveAs } from "@mui/icons-material";
+
+const Detail = () => {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const loading = document.getElementsByClassName("loading");
+  const loadingOverlay = document.getElementsByClassName("loading-overlay");
+
+  window.onload = () => {
+    loading[0].classList.add("block");
+    loadingOverlay[0].classList.add("block");
+  };
+  setTimeout(() => {
+    loading[0].classList.remove("block");
+    loadingOverlay[0].classList.remove("block");
+  }, 1100);
+
+  useEffect(() => {
+    //ChartJs
+    const ctx = document.getElementById("chart").getContext("2d");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: [
+          "20/03",
+          "21/03",
+          "22/03",
+          "23/03",
+          "24/03",
+          "25/03",
+          "26/03",
+          "27/03",
+          "28/03",
+        ],
+        datasets: [
+          {
+            data: [
+              9000000, 40000000, 70000000, 30000000, 90000000, 60000000,
+              110000000, 12000000, 120000000,
+            ],
+            label: "Icool",
+            borderColor: "#ccc",
+            backgroundColor: "#C9F4E8",
+            fill: true,
+          },
+        ],
+      },
+      options: {
+        title: {
+          display: true,
+          text: "World population per region (in millions)",
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return tooltipItem.yLabel;
+            },
+          },
+        },
+      },
+    });
+    // console.log(myChart.data.datasets[0].data);
+    const btn = document.getElementsByClassName("detail-btn");
+    function download() {
+      axios
+        .get("http://192.168.137.1:3000/todo/export", {
+          responseType: "blob",
+        })
+        .then((res) => {
+          fileDownload(res.data, "file.xlsx");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    btn[0].addEventListener("click", () => {
+      download();
+    });
+  }, []);
+
+  return (
+    <div className="detail">
+      <div className="container-fluid detail__wrap ">
+        <div className="detail__body">
+          <div className="col-md-12 col-lg-3 detail__left">
+            <FormControl fullWidth>
+              <InputLabel
+                id="demo-simple-select-label"
+                className="detail-input__title"
+              >
+                Cửa hàng
+              </InputLabel>
+              <NativeSelect
+                inputProps={{
+                  name: "age",
+                  id: "demo-simple-select",
+                }}
+                onChange={handleChange}
+                defaultValue={10}
+              >
+                <option value={10}>Ung Văn Khiêm</option>
+                <option value={20}>Twenty</option>
+                <option value={30}>Thirty</option>
+              </NativeSelect>
+            </FormControl>
+            <div className="detail-time">
+              <div className="detail-title">Thời gian dự đoán</div>
+              <Stack component="form" noValidate spacing={3}>
+                <TextField
+                  id="date"
+                  label="Từ ngày"
+                  type="date"
+                  defaultValue="2022-03-20"
+                  sx={{ width: 220 }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="date"
+                  label="Đến ngày"
+                  type="date"
+                  defaultValue="2022-03-28"
+                  sx={{ width: 220 }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Stack>
+            </div>
+
+            <Button variant="contained" className="detail-btn">
+              Download
+            </Button>
+          </div>
+          <div className="loading-overlay">
+            <div className="spinner-border loading" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+          <div className="col-md-12 col-lg-2"></div>
+          <div className="col-md-12 col-lg-7 detail__chart">
+            <div className="detail__chart--title">Biểu đồ dự đoán</div>
+            <div className="detail__chart--desc">Doanh thu</div>
+            <canvas id="chart" width="400" height="400"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "./home.scss";
+
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+import baseReq from "../../api/baseReq";
+
+const Home = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  let navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    var data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    baseReq
+      .post("login", data)
+      .then(function (response) {
+        console.log(response);
+        window.localStorage.setItem("token", response.data.token);
+        window.localStorage.setItem("user", JSON.stringify(response.data));
+        navigate("/detail");
+        window.location.reload(true);
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 10);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        if (!email || !password) {
+          alert(error.response.data.message);
+        } else {
+          alert("Incorrect account or password!!");
+        }
+        setError(error.response.data);
+      });
+  };
+  return (
+    <div className="home container">
+      <div className="home__form">
+        <h1 className="home__title">ĐĂNG NHẬP</h1>
+        <div className="loading-overlay">
+          <div className="spinner-border loading" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+        <form id="form" onSubmit={handleLogin}>
+          <TextField
+            id="outlined-basic"
+            className="home__input-user"
+            label="Tài khoản"
+            variant="outlined"
+            type="email"
+            placeholder="nhatbd"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Mật khẩu"
+            className="home__input-password"
+            variant="outlined"
+            type="password"
+            placeholder="*****************"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Nhớ mật khẩu"
+            className="home__input-checkbox"
+          />
+          <Button variant="contained" className="home-btn" type="submit">
+            Đăng nhập
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
